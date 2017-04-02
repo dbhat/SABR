d082050dabcbb5a99e691ce9294f45c05e6d7fe6
Divyashri Bhat <divyashri.bhat@gmail.com>
2017-04-03 00:02:06 +0530

    update

---
+++ results_parse/getmultipleruns_BOLA.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+mkdir dash_runtime1
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime1/
+mkdir dash_runtime2
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime2/
+mkdir dash_runtime3
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime3/
+mkdir dash_runtime4
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime4/
+mkdir dash_runtime5
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime5/
+mkdir dash_buffer1
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer1
+mkdir dash_buffer2
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer2
+mkdir dash_buffer3
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer3
+mkdir dash_buffer4
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer4
+mkdir dash_buffer5
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer5
+mkdir server_log1
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log1
+mkdir server_log2
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log2
+mkdir server_log3
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log3
+mkdir server_log4
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log4
+mkdir server_log5
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log5
+
+mkdir dash_runtime6
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime6
+mkdir dash_runtime7
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime7
+mkdir dash_runtime8
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime8
+mkdir dash_runtime9
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime9
+mkdir dash_runtime10
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime10
+mkdir dash_buffer6
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer6
+mkdir dash_buffer7
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer7
+mkdir dash_buffer8
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer8
+mkdir dash_buffer9
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer9
+mkdir dash_buffer10
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer10
+mkdir server_log6
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log6
+mkdir server_log7
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log7
+mkdir server_log8
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log8
+mkdir server_log9
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log9
+mkdir server_log10
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log10
+
+mkdir dash_runtime11
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime11
+mkdir dash_runtime12
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime12
+mkdir dash_runtime13
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime13
+mkdir dash_runtime14
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime14
+mkdir dash_runtime15
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime15
+mkdir dash_buffer11
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer11
+mkdir dash_buffer12
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer12
+mkdir dash_buffer13
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer13
+mkdir dash_buffer14
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer14
+mkdir dash_buffer15
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer15
+mkdir server_log11
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log11
+mkdir server_log12
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log12
+mkdir server_log13
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log13
+mkdir server_log14
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log14
+mkdir server_log15
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log15
+
+mkdir dash_runtime16
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime16
+mkdir dash_runtime17
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime17
+mkdir dash_runtime18
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime18
+mkdir dash_runtime19
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime19
+mkdir dash_runtime20
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_RUNTIME_LOG_* dash_runtime20
+mkdir dash_buffer16
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer16
+mkdir dash_buffer17
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer17
+mkdir dash_buffer18
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer18
+mkdir dash_buffer19
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer19
+mkdir dash_buffer20
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/DASH_BUFFER_LOG_* dash_buffer20
+mkdir server_log16
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log16
+mkdir server_log17
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log17
+mkdir server_log18
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log18
+mkdir server_log19
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log19
+mkdir server_log20
+scp -i ~/.ssh/<key> -P <portnumber> <user>@<cloudlab_client_IP>:/users/<user>/astream_dash_bolao/ASTREAM_LOGS/SERVER_LOG_* server_log20
+
