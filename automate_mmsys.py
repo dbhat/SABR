c9fc38626af77ce07d9735ba384282d025e78010
Divyashri Bhat <divyashri.bhat@gmail.com>
2017-04-02 23:05:59 +0530

    update

--- automate_mmsys.py
+++ automate_mmsys.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 '''
-This is a Python script which is used to automate AStream client and
-cross traffic generators for SABR evaluation.
+This is a Python script which is used to automate AStream clients for SABR evaluation.
 '''
 from fabric.api import *
 import random
@@ -27,40 +26,40 @@ import threading
 import os
 import time
 from collections import defaultdict
-user="dbhat0"
-
-MAX_TRIALS = 1
+user = <user name for cloudlab>
+MAX_TRIALS = <Integer of no. of experiments>
 zipf_dist = dict()
-client_ip = ["clnode080.clemson.cloudlab.us","clnode064.clemson.cloudlab.us", "clnode064.clemson.cloudlab.us","clnode064.clemson.cloudlab.us","clnode057.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode059.clemson.cloudlab.us","clnode059.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode047.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode096.clemson.cloudlab.us"]
-client_ports = [30267, 30274, 30273, 30275, 30273,30270,30271,30270,30271,30272,30267,30270,30271,30272,30268,30271,30272,30273,30273,30274]
-client_hosts_zipf = ["clnode080.clemson.cloudlab.us","clnode064.clemson.cloudlab.us", "clnode064.clemson.cloudlab.us","clnode064.clemson.cloudlab.us","clnode057.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode059.clemson.cloudlab.us","clnode059.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode047.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode096.clemson.cloudlab.us","clnode096.clemson.cloudlab.us"]
-server_ip=["clnodevm064-1.clemson.cloudlab.us","clnodevm064-2.clemson.cloudlab.us","clnodevm059-1.clemson.cloudlab.us","clnodevm096-1.clemson.cloudlab.us"]
+client_ip=[<list of client IPs>]
+client_ports = [<list of client ports>]
+client_hosts_zipf = [<list of client IPs>]
+server_ip=[<list of server IPs>]
 client_hosts =[]
 
-client_hosts1 = ["clnode054.clemson.cloudlab.us","clnode068.clemson.cloudlab.us","clnode068.clemson.cloudlab.us","clnode054.clemson.cloudlab.us","clnode054.clemson.cloudlab.us"]
-client_hosts2 = ["clnode082.clemson.cloudlab.us","clnode082.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode082.clemson.cloudlab.us","clnode082.clemson.cloudlab.us"]
-client_hosts3 = ["clnode080.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode080.clemson.cloudlab.us","clnode051.clemson.cloudlab.us","clnode082.clemson.cloudlab.us"]
-client_hosts4 = ["clnode068.clemson.cloudlab.us","clnode051.clemson.cloudlab.us","clnode068.clemson.cloudlab.us","clnode068.clemson.cloudlab.us","clnode068.clemson.cloudlab.us"]
-#cl_comm =["\"cd /users/dbhat0/AStream; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(s)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(s)+1)+ ".mpd -p sara > /dev/null &\"","\"cd /users/dbhat0/astream_dash_clab; python dist/client/dash_client_cachemap.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(s)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(s)+1)+ ".mpd -p vlc > /users/dbhat0/dash_"+str(ipaddress).replace(".","")+"2>&1\""]
+client_hosts1 = [<list of client IPs Group A>]
+client_hosts2 = [<list of client IPs Group B>]
+client_hosts3 = [<list of client IPs Group C>]
+client_hosts4 = [<list of client IPs Group D>]
 
 def gen_zipf(a,n):
     s= np.random.zipf(a,n)
     result = (s/float(max(s)))*n
     return np.floor(result)
-
+'''
+This API is used to reset caches before every run
+'''
 def dash_server(ipaddress,run):
     global user
      
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(ipaddress,username=user,password="Platinum*1989",key_filename="/users/dbhat/.ssh/id_geni_ssh_rsa")
+        ssh.connect(ipaddress,username=user,password=<password for CloudLab key goes here>,key_filename=<path to ssh key>)
     except paramiko.AuthenticationException:
         print "[-] Authentication Exception! ..."      
          
     except paramiko.SSHException:
         print "[-] SSH Exception! ..." 
-         
+    
     works = ipaddress.strip('\n')+','+user  
     print '[+] '+ works
     if run == 0:
@@ -70,23 +69,26 @@ def dash_server(ipaddress,run):
 	elif cl_comm == 2:
     	stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus squaddump/cachestatus/cache1.bson")    	
     elif cl_comm == 3:
-    	stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus squad_sabrdump2/cachestatus/cache1.bson")
+    	stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus squad_sabrdump/cachestatus/cache1.bson")
     elif cl_comm == 4:
-    	stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus boladump2/cachestatus/cache1.bson")
+    	stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus boladump/cachestatus/cache1.bson")
     elif cl_comm == 5:
-		stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus sabrboladump2/cachestatus/cache1.bson")
+		stdin,stdout,stderr=ssh.exec_command("mongorestore --collection cache1 --db cachestatus sabrboladump/cachestatus/cache1.bson")
 
     print stdout.read()						
     print stderr.read()
     ssh.close()
 
+'''
+    This API is used to run different clients with and without SABR modifications
+'''
 def dash_client(ipaddress, ports, zipf_index, mpd_ip, cl_comm):
     global user
      
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(ipaddress,username=user,port=ports,password="Platinum*1989",key_filename="/users/dbhat/.ssh/id_geni_ssh_rsa")
+        ssh.connect(ipaddress,username=user,port=ports,password=<password for CloudLab key goes here>,key_filename=<path to ssh key>)
     except paramiko.AuthenticationException:
         print "[-] Authentication Exception! ..."      
          
@@ -108,8 +110,7 @@ def dash_client(ipaddress, ports, zipf_index, mpd_ip, cl_comm):
 		cl_command = "cd /users/dbhat0/astream_dash_bola; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(s)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(s)+1)+ ".mpd -p bola > /dev/null &"
     elif cl_comm == 5:
 		cl_command = "cd /users/dbhat0/astream_sabr_bola; python dist/client/dash_client.py -m http://"+str(mpd_ip)+"/BigBuckBunny_2s_mod" + str(int(s)+1) + "/www-itec.uni-klu.ac.at/ftp/datasets/DASHDataset2014/BigBuckBunny/2sec/BigBuckBunny_2s_mod" +str(int(s)+1)+ ".mpd -p bola > /dev/null &"
-    
-    #cl_command="rm /users/dbhat0/astream_sabr_bola/ASTREAM_LOGS/*; rm /users/dbhat0/astream_dash_bola/ASTREAM_LOGS/*; rm /users/dbhat0/astream_dash_clab/ASTREAM_LOGS/*; rm /users/dbhat0/AStream/ASTREAM_LOGS/*; rm /users/dbhat0/astream_dash_squad/ASTREAM_LOGS/*;rm /users/dbhat0/AStream_SQUAD/ASTREAM_LOGS/*"
+
     stdin,stdout,stderr=ssh.exec_command(cl_command)
     stringout = str(stderr.readlines()) + str(ipaddress) + str(ports)
     print stringout
@@ -122,18 +123,18 @@ if __name__ == "__main__":
  for run in range (0,6):
  	for ip in server_ip:	
 		try:
-				client=pymongo.MongoClient(ip)#Replace with IP of specific server
+				client=pymongo.MongoClient(ip)
 				print"Connected successfully again!!!"
 		except pymongo.errors.ConnectionFailure, e:
 				print "Could not connect to MongoDB sadly"
 		db = client.cachestatus
 		table = db.cache1
-		table.delete_many({})
+        table.delete_many({})#Reset caches before each run
 		dash_server(str(ip),run)
 	try:
-	    
+            #Client chooses video from a set of 50 using the zipf distribution
 	        for client in client_hosts_zipf:
-			zipf_dist[client] = gen_zipf(2,49)
+                zipf_dist[client] = gen_zipf(2,49)
 			print zipf_dist[client], client
 
 		
